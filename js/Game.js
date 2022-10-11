297a4eda3f70db19a8e5ba053d02afb507a26f11
mariadlcruzz0-0 <mariadlcruzz0.0@gmail.com>
2022-10-11 12:13:53 -0700

    created two files. One is the game file and the phrases file. Each file has it's own class and constructor.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,23 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+           new Phrase('phrase 1'),
+           new Phrase('phrase 2'),
+           new Phrase('phrase 3'),
+           new Phrase('phrase 4'),
+           new Phrase('phrase 5') 
+        ];
+        this.activePhrase = null;
+    }
+    startGame() {
+        this.activePhrase = this.getRandomPhrase();
+    }
+    getRandomPhrase() {
+        this.phrases = Math.random();
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,23 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    //Functions
+    addPhraseToDisplay(){
+
+    
+    }
+
+    checkLetter() {
+
+    }
+    showMatchedLetter(){
+
+        
+    }
+}
